44438b418cfc50b26eb7e3ee6647f28eda3f21fb
Mogoson <mogoson@outlook.com>
2021-11-27 22:39:59 +0800

    Add CommandParser

---
+++ UnityProject/Assets/MGS.Packages/CommandServo/Runtime/Framework/CommandParser.cs
@@ -0,0 +1,38 @@
+﻿/*************************************************************************
+ *  Copyright © 2021 Mogoson. All rights reserved.
+ *------------------------------------------------------------------------
+ *  File         :  CommandParser.cs
+ *  Description  :  Command Parser.
+ *------------------------------------------------------------------------
+ *  Author       :  Mogoson
+ *  Version      :  0.1.0
+ *  Date         :  4/6/2021
+ *  Description  :  Initial development version.
+ *************************************************************************/
+
+using System.Collections.Generic;
+
+namespace MGS.CommandServo
+{
+    /// <summary>
+    /// Command Parser.
+    /// </summary>
+    public abstract class CommandParser : ICommandParser
+    {
+        #region Public Method
+        /// <summary>
+        /// Parser byte buffer to commands.
+        /// </summary>
+        /// <param name="buffer">Buffer to parse.</param>
+        /// <returns>Commands from buffer.</returns>
+        public abstract byte[] ToBuffer(Command command);
+
+        /// <summary>
+        /// Parser command to byte buffer.
+        /// </summary>
+        /// <param name="command">Command to parse.</param>
+        /// <returns>Buffer from command.</returns>
+        public abstract IEnumerable<Command> ToCommands(byte[] buffer);
+        #endregion
+    }
+}
